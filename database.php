00636fd44ac6a8860ff399505a45b70d89a5d757
UncleBinary1001 <nzundamahlangu@gmail.com>
2021-02-03 14:35:33 +0200

    Added remote database credentials

--- database.php
+++ database.php
@@ -1,13 +1,20 @@
 <?php
 //Create connection credentials
-$db_host = "localhost";
+/*&$db_host = "localhost";
 $db_name = "quizzer";
 $db_user =  "root";
-$db_pass = "";
+$db_pass = "";*/
+//Remote database connection credentials
+$db_host = "remotemysql.com";
+$db_name = "NhQd82hxIE";
+$db_user =  "8aIOzEjsKx";
+$db_pass = "NhQd82hxIE";
 
 //Create mySqli object
 
 $mysqli = new mysqli($db_host,$db_user,$db_pass,$db_name);
+
+
 
 //Error Handler
 if($mysqli->connect_error){
